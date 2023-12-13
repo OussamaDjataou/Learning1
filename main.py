89cc08a957e196bffa1cc56aaf134a420419183c
OussamaDjataou <56490553+OussamaDjataou@users.noreply.github.com>
2023-12-13 12:35:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    app.run(host="0.0.0.0", port=3000)
-    return "<p>Hello, World!</p>"
+print("third test")
